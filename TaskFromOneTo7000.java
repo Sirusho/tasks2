7c8f826c1832def8c30d1ee982a616ccb35dba4a
Siranush Kamavosyan <sirushokamavosyan@gmail.com>
2021-05-05 11:50:35 +0400

    tasks added

---
+++ TaskFromOneTo7000.java
@@ -0,0 +1,16 @@
+public class TaskFromOneTo7000 {
+    public static void main(String[] args) {
+        int arr[]=new int[1000];
+        int i=0;
+        while (i<arr.length){
+            for (int j = 1; j <= 7000; j++) {
+                if (j % 7 == 0) {
+                    arr[i] = j;
+                    System.out.print(arr[i] + " ");
+                    i++;
+                }
+            }
+        }
+
+    }
+}
